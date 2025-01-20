e74fdc28194f85a33489dbc7d005c0e10c6a1cb2
hugo <hgonzalez3004@gmail.com>
2025-01-20 23:46:13 +0100

    page de connexion

---
+++ Qui_fait_quoi_quand/src/Controller/LoginController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+final class LoginController extends AbstractController
+{
+    #[Route('/Login', name: 'app_login')]
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+        // Récupère l'erreur de connexion s'il y en a
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // Récupère le dernier nom d'utilisateur saisi
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('login/index.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+}
