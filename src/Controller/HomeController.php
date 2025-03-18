eba580063e7589ec7c19b98b84836ac3365b1b7b
hugo <hgonzalez3004@gmail.com>
2025-03-18 12:47:51 +0100

    test serveur render47

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,9 +5,30 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Entity\User;
+
 
 final class HomeController extends AbstractController
+
 {
+
+    
+
+    #[Route('/test-login', name: 'app_test_login')]
+    public function testLogin(EntityManagerInterface $em, UserPasswordHasherInterface $passwordHasher)
+    {
+        $user = $em->getRepository(User::class)->findOneBy(['email' => 'referent1@example.com']);
+
+        if (!$user) {
+            return new Response('Utilisateur non trouvé');
+        }
+
+        $isValid = $passwordHasher->isPasswordValid($user, 'password123');
+
+        return new Response($isValid ? 'Connexion OK' : 'Mot de passe incorrect');
+    }
     #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
