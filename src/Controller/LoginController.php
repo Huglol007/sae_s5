4aae8cfb5a1915fbe0807dd29b905963f422f616
hugo <hgonzalez3004@gmail.com>
2025-03-18 15:45:10 +0100

    test serveur render53

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -12,23 +12,14 @@ final class LoginController extends AbstractController
     #[Route('/Login', name: 'app_login')]
     public function index(AuthenticationUtils $authenticationUtils): Response
     {
-        // 🔍 Debug : Vérifier si le contrôleur est bien appelé
-        dump($_SERVER); die();
         // Récupère l'erreur de connexion s'il y en a
         $error = $authenticationUtils->getLastAuthenticationError();
-
-        // 🔍 Debug : Voir l'erreur d'authentification
 
         // Récupère le dernier nom d'utilisateur saisi
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        // 🔍 Debug : Voir l'utilisateur récupéré
-        ($lastUsername);
-
-        // Arrêter l'exécution ici pour voir les dumps
 
         return $this->render('login/index.html.twig', [
-
             'last_username' => $lastUsername,
             'error' => $error,
         ]);
